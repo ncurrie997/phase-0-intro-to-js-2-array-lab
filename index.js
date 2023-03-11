bab0da34c21a77e0fd862ae1ee441d5e114eeb29
Nick Currie <gosox997@gmail.com>
2023-03-11 03:33:49 -0500

    All tests Passed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+]
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const appNewCats = [...cats, "Broom"]
+    return appNewCats;
+}
+
+function prependCat(name) {
+    const preNewCat = ["Arnold", ...cats];
+    return preNewCat;
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+    
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
